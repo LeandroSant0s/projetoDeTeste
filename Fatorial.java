9a16dc5ae454d7729f4b80246433eb9f4c234dee
Leandro Alves dos Santos <leandrosantos.info@gmail.com>
2015-05-28 20:36:35 -0300

    Alteracao para o calculo correto do fatorial zero

--- Fatorial.java
+++ Fatorial.java
@@ -4,10 +4,10 @@ public class Fatorial{
          if (n<0)
                System.out.println("Error");
          int resultado = 1;
-         
+         if(n!=0){
          for(int i = 2; i<=n; i++)
             resultado *= i;
-
+}     
             return resultado;      
      
     }
