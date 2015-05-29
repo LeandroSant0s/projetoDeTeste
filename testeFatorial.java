61c01d710cc9e1d803d8fb74bb8c8a06d4198249
Leandro Alves dos Santos <leandrosantos.info@gmail.com>
2015-05-28 22:38:27 -0300

    Comentario inserido em testeFatorial.java

--- testeFatorial.java
+++ testeFatorial.java
@@ -1,4 +1,5 @@
 class testeFatorial{
+         //Comentario inserido em testeFatorial.java
          public static void main(String[] args) {
                Fatorial fat = new Fatorial();
                int r = fat.calculaFatorial(0);
